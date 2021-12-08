4ae8df1b1c892c571a4ca75f11ce14b3363f1d03
Zach Stone <zachinjapan888@gmail.com>
2021-12-07 22:43:50 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Header, StyleSheet, Text, View, Button } from "react-native";
+
+const randomHexColor = () => {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
+  const [backgroundColor1, setBackgroundColor1] = React.useState("#000000");
+  const combineStyles = StyleSheet.flatten([styles]);
+  const [timer, setTimer] = React.useState(null);
+
+  const startTimer = () => {
+    setTimer(
+      setInterval(() => {
+        setCount;
+      }, 1000)
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={[combineStyles.container, { backgroundColor: backgroundColor1 }]}
+    >
+      <Text style={styles.title}>
+        click the button 10 times in 10 seconds to change the background color
+      </Text>
+      <Text style={styles.count}>{count}</Text>
+
+      <Button
+        onPress={() => {
+          if (count === 10) {
+            setBackgroundColor1(randomHexColor());
+            setCount(0);
+          } else {
+            setCount(count + 1);
+          }
+        }}
+        title="click me"
+        color="#841584"
+        style={styles.button}
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +51,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgb(23, 0, 0)",
+    color: "white",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignContent: "center",
+    alignItems: "center",
+  },
+
+  count: {
+    fontSize: 30,
+    color: "white",
+  },
+
+  title: {
+    fontSize: 40,
+    color: "white",
+  },
+
+  button: {
+    width: 200,
+    height: 50,
+    backgroundColor: "rgb(0, 0, 0)",
   },
 });
