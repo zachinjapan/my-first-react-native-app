6a51e73fef979a9872c59ce5a12d5c0519afd204
Zach Stone <zachinjapan888@gmail.com>
2021-12-08 07:27:08 -0500

    made the countdown timer work

--- App.js
+++ App.js
@@ -10,39 +10,57 @@ export default function App() {
   const [count, setCount] = React.useState(0);
   const [backgroundColor1, setBackgroundColor1] = React.useState("#000000");
   const combineStyles = StyleSheet.flatten([styles]);
-  const [timer, setTimer] = React.useState(null);
+  const [seconds, setSeconds] = React.useState(5);
+  const [isRunning, setIsRunning] = React.useState(false);
 
-  const startTimer = () => {
-    setTimer(
-      setInterval(() => {
-        setCount;
-      }, 1000)
-    );
-  };
+  //  looping timer that counts down from 10 seconds and then sets count to 0 and restarts the timer
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds - 1);
+      if (seconds === 0) {
+        setCount(0);
+        setIsRunning(false);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [seconds]);
 
   return (
     <View
       style={[combineStyles.container, { backgroundColor: backgroundColor1 }]}
     >
       <Text style={styles.title}>
-        click the button 10 times in 10 seconds to change the background color
+        {isRunning === false
+          ? "click the button 30 times in 5 seconds for a surprise"
+          : `${seconds} seconds left`}
       </Text>
       <Text style={styles.count}>{count}</Text>
-
-      <Button
-        onPress={() => {
-          if (count === 10) {
-            setBackgroundColor1(randomHexColor());
-            setCount(0);
-          } else {
-            setCount(count + 1);
-          }
-        }}
-        title="click me"
-        color="#841584"
-        style={styles.button}
-        accessibilityLabel="Learn more about this purple button"
-      />
+      {isRunning === false ? (
+        <Button
+          title="Start"
+          onPress={() => {
+            setIsRunning(true);
+            setSeconds(5);
+          }}
+        />
+      ) : (
+        <Button
+          onPress={() => {
+            if (isRunning === true && count <= 28) {
+              setCount(count + 1);
+            } else if (isRunning === true && count === 29) {
+              setCount(0);
+              setIsRunning(false);
+              setBackgroundColor1(randomHexColor());
+            }
+          }}
+          title="click me"
+          color="#841584"
+          style={styles.button}
+          accessibilityLabel="Learn more about this purple button"
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
